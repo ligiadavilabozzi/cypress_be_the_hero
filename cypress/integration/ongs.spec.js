e8818436185973e2c7a70642e136fa565c84218b
ligia.bozzi@gmail.com <ligia.bozzi@gmail.com>
2021-11-05 08:00:28 -0300

    implementing test register with cypress

--- cypress/integration/ongs.spec.js
+++ cypress/integration/ongs.spec.js
@@ -4,20 +4,23 @@ describe('Ongs',()=>{
     it('The Ongs must be able to register.', ()=>{
     
         cy.visit('http://localhost:3000/register')
+        cy.get('[data-cy=name]').type('Dogs & Cats Lovers')
         cy.get('[data-cy=email]').type('dogsandcats@gmail.com')
-        cy.get('[data-cy=whatsapp]').type('1199999999')
-        cy.get('[data-cy=cidade]').type('São Paulo')
+        cy.get('[data-cy=whatsapp]').type('11999999999')
+        cy.get('[data-cy=city]').type('São Paulo')
         cy.get('[data-cy=uf]').type('SP')
+
+        
+        cy.intercept('POST', '**/ongs').as('postOng')
 
         cy.get('[data-cy=submit]').click()
 
-        cy.intercept('POST', '**/ongs').as('postOng')
 
-        cy.wait('@postOng').then(({xhr}) =>{
-            expect(xhr.statusCode).be.equal(200)
-        })
+        cy.wait('@postOng').its('response.statusCode').should('eq',200)
 
-    }); 
+    })
+
+
 
         
 
