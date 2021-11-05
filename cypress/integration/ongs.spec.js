e09ee3ca1fe910ba2a12fe4cdc74bcf468a4680b
ligia.bozzi@gmail.com <ligia.bozzi@gmail.com>
2021-11-05 06:32:48 -0300

    testing register

--- cypress/integration/ongs.spec.js
+++ cypress/integration/ongs.spec.js
@@ -1,2 +1,29 @@
 /// <reference types="cypress" /> 
 
+describe('Ongs',()=>{
+    it('The Ongs must be able to register.', ()=>{
+    
+        cy.visit('http://localhost:3000/register')
+        cy.get('[data-cy=email]').type('dogsandcats@gmail.com')
+        cy.get('[data-cy=whatsapp]').type('1199999999')
+        cy.get('[data-cy=cidade]').type('São Paulo')
+        cy.get('[data-cy=uf]').type('SP')
+
+        cy.get('[data-cy=submit]').click()
+
+        cy.intercept('POST', '**/ongs').as('postOng')
+
+        cy.wait('@postOng').then(({xhr}) =>{
+            expect(xhr.statusCode).be.equal(200)
+        })
+
+    }); 
+
+        
+
+        
+
+    it('The ong must be able to log into the system',()=>{
+
+    })
+})
